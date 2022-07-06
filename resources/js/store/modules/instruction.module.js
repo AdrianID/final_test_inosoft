0791d707856325b9ae27e1c17698ae97d2c22e06
AdrianID <adrianfrizna14@gmail.com>
2022-07-06 16:23:00 +0700

    Setup New Project

---
+++ resources/js/store/modules/instruction.module.js
@@ -0,0 +1,23 @@
+const state = {
+
+};
+
+const mutations = {
+    
+};
+
+const actions = {
+    
+};
+
+const getters = {
+    
+}
+
+export const instruction = {
+    namespaced: true,
+    getters,
+    state,
+    actions,
+    mutations
+};
